f4f6e91d21aa82b5a4ae0a3be252622c18026ec4
elisamantelli <55712229+elisamantelli@users.noreply.github.com>
2019-09-23 17:22:15 -0400

    Add files via upload

---
+++ run_stokes_gamma_1.m
@@ -0,0 +1,32 @@
+N = 4;
+maxNumCompThreads(N)
+
+T_bed = -0.5;
+choice = 'mono_small';
+gamma = 1;
+index_wavelength = 1;
+
+
+
+% grid parameters
+bd_x = pi/2;
+bd_z.ice = 4;            %top of the boundary layer
+bd_z.bed = -4;           %bottom of the bed boundary layer
+
+n_z.psi = 200;
+n_z.Tice = 600;
+n_z.Tbed = 600;
+n_x = 300;
+
+%timestepping parameters
+dt = 0.005;
+t_plot = 0.1;
+stepmax = 1000;
+t_init = 0;
+step_init = 1;
+
+scale_pert = 1e-03; %amplitude of initial perturbation
+
+network_script_cluster(T_bed, choice, gamma,bd_x,n_x, bd_z, n_z, dt,t_plot, stepmax,t_init, step_init, index_wavelength, scale_pert)
+
+quit
---
+++ run_stokes_gamma_5.m
@@ -0,0 +1,31 @@
+N = 8;
+maxNumCompThreads(N)
+T_bed = -0.5;
+choice = 'wn';
+gamma = 5;
+index_wavelength = 2;
+
+
+
+% grid parameters
+bd_x = 4*pi;
+bd_z.ice = 4;           %top of the boundary layer
+bd_z.bed = -4;          %bottom of the bed boundary layer
+
+n_z.psi = 100;
+n_z.Tice = 100;
+n_z.Tbed = 100;
+n_x = 1050;
+
+upwind = 1/2; %set to zero for upwinding hor fluxes in heat equation and Q equation
+
+%timestepping parameters
+dt = 7e-03;
+t_plot = 10*dt;
+stepmax = 2000;
+t_init = 0;
+step_init = 1;
+
+scale_pert = 1e-03; %amplitude of initial perturbation
+
+network_script_cluster(T_bed, choice, gamma,bd_x,n_x, bd_z, n_z, dt,t_plot, stepmax,t_init, step_init, index_wavelength, scale_pert, upwind)
